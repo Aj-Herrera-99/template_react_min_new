5a6706ea2cd3481858aa2caf0b47fe3e62c7d173
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-02-07 16:59:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-center mt-10 text-4xl">Libraries installed:</h1>
+            <ul className="text-center text-2xl">
+                <li>axios</li>
+                <li>react-router</li>
+                <li>tailwind css</li>
+            </ul>
+        </>
+    );
+}
+
+export default App;
